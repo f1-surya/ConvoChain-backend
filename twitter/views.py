622ffe973e74e17ed32dba0fcc5a5c0283439aa1
surya <balasurya811@gmail.com>
2023-03-01 15:52:57 +0530

    Comments of users followed by the user that made the request will now be returned by the Tweet serializer instead of the app's view.

--- twitter/views.py
+++ twitter/views.py
@@ -5,7 +5,6 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from comments.serializers import CommentSerializer
 from meta.models import Meta
 from retweets.serializers import ReTweetSerializer
 from tweets.serializers import TweetSerializer
@@ -22,29 +21,22 @@ class GetData(APIView):
             if metas is None:
                 metas = Meta.objects.filter(author=follow.user)
             else:
-                followed_metas = Meta.objects.filter(author=follow.user)
-                metas = chain(metas, followed_metas)
+                metas = chain(metas, Meta.objects.filter(author=follow.user))
 
         tweets = []
-        comments = []
         retweets = []
         for meta in metas:
             if meta.content_type == 'tweet':
                 tweets.append(meta.tweet)
             elif meta.content_type == 'retweet':
                 retweets.append(meta.retweet)
-            elif meta.content_type == 'comment':
-                comments.append(meta.comment)
 
         tweet_serializer = TweetSerializer(data=tweets, many=True, context={'user': request.user})
         tweet_serializer.is_valid()
 
-        comment_serializer = CommentSerializer(data=comments, many=True, context={'user': request.user})
-        comment_serializer.is_valid()
-
         retweet_serializer = ReTweetSerializer(data=retweets, many=True, context={'user': request.user})
         retweet_serializer.is_valid()
 
-        data = chain(tweet_serializer.data, comment_serializer.data, retweet_serializer.data)
+        data = chain(tweet_serializer.data, retweet_serializer.data)
         data = sorted(data, key=lambda content: content['meta']['posted_date'], reverse=True)
         return Response(data)
